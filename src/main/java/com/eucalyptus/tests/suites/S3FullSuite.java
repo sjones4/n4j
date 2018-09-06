5163ae9a77591f43040d026ba042b573629d7318
Steve Jones <steve@iaascream.cloud>
2018-09-05 21:03:59 -0700

    Add bucket policy test to s3 full suite

--- src/main/java/com/eucalyptus/tests/suites/S3FullSuite.java
+++ src/main/java/com/eucalyptus/tests/suites/S3FullSuite.java
@@ -3,19 +3,12 @@ package com.eucalyptus.tests.suites;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import com.eucalyptus.tests.awssdk.S3BucketACLTests;
-import com.eucalyptus.tests.awssdk.S3BucketTests;
+import com.eucalyptus.tests.awssdk.S3BucketPolicyTests;
 import com.eucalyptus.tests.awssdk.S3CopyObjectTests;
 import com.eucalyptus.tests.awssdk.S3CorsTests;
-import com.eucalyptus.tests.awssdk.S3ListMpuTests;
 import com.eucalyptus.tests.awssdk.S3ListObjectsTests;
 import com.eucalyptus.tests.awssdk.S3ListVersionsTests;
-import com.eucalyptus.tests.awssdk.S3MultiPartUploadTests;
-import com.eucalyptus.tests.awssdk.S3ObjectACLAcrossAccountsTests;
-import com.eucalyptus.tests.awssdk.S3ObjectACLTests;
 import com.eucalyptus.tests.awssdk.S3ObjectLifecycleTests;
-import com.eucalyptus.tests.awssdk.S3ObjectMultiDeleteTests;
-import com.eucalyptus.tests.awssdk.S3ObjectTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({
@@ -23,6 +16,7 @@ import com.eucalyptus.tests.awssdk.S3ObjectTests;
     S3ShortSuite.class,
 
     // tests
+    S3BucketPolicyTests.class,
     S3CopyObjectTests.class,
     S3CorsTests.class,
     S3ListObjectsTests.class,
