33a302db7d0d70ed4c19fbe217e6dc44551f2365
Steve Jones <steve@iaascream.cloud>
2018-09-06 17:49:16 -0700

    SQS shorter short test suite

--- src/main/java/com/eucalyptus/tests/suites/SQSFullSuite.java
+++ src/main/java/com/eucalyptus/tests/suites/SQSFullSuite.java
@@ -3,11 +3,27 @@ package com.eucalyptus.tests.suites;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import com.eucalyptus.tests.awssdk.TestSQSAnonymousAccess;
+import com.eucalyptus.tests.awssdk.TestSQSAttributeValuesInMessages;
+import com.eucalyptus.tests.awssdk.TestSQSChangeMessageVisibilityBatch;
 import com.eucalyptus.tests.awssdk.TestSQSCloudWatchMetrics;
 import com.eucalyptus.tests.awssdk.TestSQSCrossAccountStackPolicies;
 import com.eucalyptus.tests.awssdk.TestSQSDeadLetterQueue;
+import com.eucalyptus.tests.awssdk.TestSQSDelaySeconds;
+import com.eucalyptus.tests.awssdk.TestSQSDeleteMessageBatch;
 import com.eucalyptus.tests.awssdk.TestSQSIAMPolicies;
+import com.eucalyptus.tests.awssdk.TestSQSListDeadLetterSourceQueues;
 import com.eucalyptus.tests.awssdk.TestSQSLongPolling;
+import com.eucalyptus.tests.awssdk.TestSQSMessageExpirationPeriod;
+import com.eucalyptus.tests.awssdk.TestSQSPermissions;
+import com.eucalyptus.tests.awssdk.TestSQSPurgeQueue;
+import com.eucalyptus.tests.awssdk.TestSQSQueuePolicy;
+import com.eucalyptus.tests.awssdk.TestSQSQueueUrlBinding;
+import com.eucalyptus.tests.awssdk.TestSQSQuotas;
+import com.eucalyptus.tests.awssdk.TestSQSReadOnlyAttributes;
+import com.eucalyptus.tests.awssdk.TestSQSReceiveMessage;
+import com.eucalyptus.tests.awssdk.TestSQSStatusCodesForNonexistentQueues;
+import com.eucalyptus.tests.awssdk.TestSQSVisibilityTimeout;
 
 
 /**
@@ -19,11 +35,27 @@ import com.eucalyptus.tests.awssdk.TestSQSLongPolling;
     SQSShortSuite.class,
 
     // tests
+    TestSQSAnonymousAccess.class,
+    TestSQSAttributeValuesInMessages.class,
+    TestSQSChangeMessageVisibilityBatch.class,
     TestSQSCloudWatchMetrics.class,
     TestSQSCrossAccountStackPolicies.class,
     TestSQSDeadLetterQueue.class,
+    TestSQSDelaySeconds.class,
+    TestSQSDeleteMessageBatch.class,
     TestSQSIAMPolicies.class,
+    TestSQSListDeadLetterSourceQueues.class,
     TestSQSLongPolling.class,
+    TestSQSMessageExpirationPeriod.class,
+    TestSQSPermissions.class,
+    TestSQSPurgeQueue.class,
+    TestSQSQueuePolicy.class,
+    TestSQSQueueUrlBinding.class,
+    TestSQSQuotas.class,
+    TestSQSReadOnlyAttributes.class,
+    TestSQSReceiveMessage.class,
+    TestSQSStatusCodesForNonexistentQueues.class,
+    TestSQSVisibilityTimeout.class,
 })
 public class SQSFullSuite {
   // junit test suite as defined by SuiteClasses annotation       
--- src/main/java/com/eucalyptus/tests/suites/SQSShortSuite.java
+++ src/main/java/com/eucalyptus/tests/suites/SQSShortSuite.java
@@ -4,62 +4,30 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import com.eucalyptus.tests.awssdk.TestSQSAdminFunctions;
-import com.eucalyptus.tests.awssdk.TestSQSAnonymousAccess;
-import com.eucalyptus.tests.awssdk.TestSQSAttributeValuesInMessages;
 import com.eucalyptus.tests.awssdk.TestSQSAttributes;
 import com.eucalyptus.tests.awssdk.TestSQSChangeMessageVisibility;
-import com.eucalyptus.tests.awssdk.TestSQSChangeMessageVisibilityBatch;
 import com.eucalyptus.tests.awssdk.TestSQSCreateQueue;
-import com.eucalyptus.tests.awssdk.TestSQSDelaySeconds;
 import com.eucalyptus.tests.awssdk.TestSQSDeleteMessage;
-import com.eucalyptus.tests.awssdk.TestSQSDeleteMessageBatch;
 import com.eucalyptus.tests.awssdk.TestSQSDeleteQueue;
 import com.eucalyptus.tests.awssdk.TestSQSGetQueueUrl;
-import com.eucalyptus.tests.awssdk.TestSQSListDeadLetterSourceQueues;
 import com.eucalyptus.tests.awssdk.TestSQSListQueues;
-import com.eucalyptus.tests.awssdk.TestSQSMessageExpirationPeriod;
-import com.eucalyptus.tests.awssdk.TestSQSPermissions;
-import com.eucalyptus.tests.awssdk.TestSQSPurgeQueue;
-import com.eucalyptus.tests.awssdk.TestSQSQueuePolicy;
-import com.eucalyptus.tests.awssdk.TestSQSQueueUrlBinding;
-import com.eucalyptus.tests.awssdk.TestSQSQuotas;
-import com.eucalyptus.tests.awssdk.TestSQSReadOnlyAttributes;
-import com.eucalyptus.tests.awssdk.TestSQSReceiveMessage;
 import com.eucalyptus.tests.awssdk.TestSQSSendMessage;
 import com.eucalyptus.tests.awssdk.TestSQSSendMessageBatch;
 import com.eucalyptus.tests.awssdk.TestSQSSenderId;
-import com.eucalyptus.tests.awssdk.TestSQSStatusCodesForNonexistentQueues;
-import com.eucalyptus.tests.awssdk.TestSQSVisibilityTimeout;
 
 @RunWith(Suite.class)
 @SuiteClasses({
     TestSQSAdminFunctions.class,
-    TestSQSAnonymousAccess.class,
     TestSQSAttributes.class,
-    TestSQSAttributeValuesInMessages.class,
     TestSQSChangeMessageVisibility.class,
-    TestSQSChangeMessageVisibilityBatch.class,
     TestSQSCreateQueue.class,
-    TestSQSDelaySeconds.class,
     TestSQSDeleteMessage.class,
-    TestSQSDeleteMessageBatch.class,
     TestSQSDeleteQueue.class,
     TestSQSGetQueueUrl.class,
-    TestSQSListDeadLetterSourceQueues.class,
     TestSQSListQueues.class,
-    TestSQSMessageExpirationPeriod.class,
-    TestSQSPermissions.class,
-    TestSQSPurgeQueue.class,
-    TestSQSQueuePolicy.class,
-    TestSQSQueueUrlBinding.class,
-    TestSQSQuotas.class,
-    TestSQSReadOnlyAttributes.class,
-    TestSQSReceiveMessage.class,
     TestSQSSenderId.class,
     TestSQSSendMessage.class,
     TestSQSSendMessageBatch.class,
-    TestSQSStatusCodesForNonexistentQueues.class,
-    TestSQSVisibilityTimeout.class,
 })
 public class SQSShortSuite {
   // junit test suite as defined by SuiteClasses annotation
